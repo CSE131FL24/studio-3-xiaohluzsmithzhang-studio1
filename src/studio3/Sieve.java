498c90bef07c1d48a60ec6290d2119511a52b295
evanxiao <evanxiao@localhost>
2024-09-19 11:23:44 -0500

    studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,23 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+
+Scanner in = new Scanner(System.in);
+		
+		System.out.println("Give me a positive integer UwU: ");
+		int userInteger = in.nextInt();
+		boolean [] array = new boolean [userInteger];
+		boolean 
+				
+		//for ( int i = 0; i < userInteger; i++) {
+			 [i]++;
+		}
+		//System.out.println(array);
+
+	}
+
+}
